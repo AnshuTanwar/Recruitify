14db5f657e3cf9d6bf4b08549babdaaab073cba0
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-08-29 13:19:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Landing from './shared/pages/Landing.jsx';
@@ -72,39 +71,3 @@ function App() {
 }
 
 export default App;
-
-
-=======
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Landing from './pages/Landing.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
-import AnimatedBackground from './components/AnimatedBackground.jsx';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-  
-function App() {
-  return (
-    <Router>
-      <div className="relative min-h-screen overflow-hidden">
-        <AnimatedBackground />
-        <Header />
-        <AnimatePresence mode="wait">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </AnimatePresence>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-
-
-
->>>>>>> c60762e934cc3f975aecd7e2283106669e5adef2
