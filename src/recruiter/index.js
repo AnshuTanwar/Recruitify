f149d516d333e1e21683a7d0cc28e7b31c6c468f
Deepak <158290849+Deepak0880@users.noreply.github.com>
2025-09-05 16:41:56 +0530

    Update index.js

--- src/recruiter/index.js
+++ src/recruiter/index.js
@@ -4,6 +4,6 @@ export { default as RecruiterJobs } from './pages/RecruiterJobs.jsx';
 export { default as PostJob } from './pages/PostJob.jsx';
 export { default as RecruiterSignup } from './pages/RecruiterSignup.jsx';
 
-// Recruiter Components
+// Recruiter Components  
 export { default as RecruiterLayout } from './components/RecruiterLayout.jsx';
 export { default as RecruiterSidebar } from './components/RecruiterSidebar.jsx';
